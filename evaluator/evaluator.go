941aa82a6dc797e4a623e495f054ed8b0ecdd6cd
sthtnr <beautifulwindmi11@gmail.com>
2019-08-09 20:51:10 +0900

    feat: implementation for evaluating boolean literal

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -18,6 +23,9 @@ func Eval(node ast.Node) object.Object {
 	// 式
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBooltoBooleanObject(node.Value)
 	}
 
 	return nil
@@ -31,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBooltoBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
