ecfe17b68d786925d2b13efdeb6f496cf393ec1c
sthtnr <beautifulwindmi11@gmail.com>
2019-08-10 16:50:16 +0900

    feat: implementation for evaluating if-else expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -30,6 +30,12 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
 	// 式
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -127,5 +133,30 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBooltoBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
