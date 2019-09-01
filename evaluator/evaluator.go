813c2a2fd19c671d53e1e09b4baa33b10a5fc571
sthtnr <beautifulwindmi11@gmail.com>
2019-09-01 12:27:30 +0900

    feat: implementation of concatenation of strings

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -187,6 +187,8 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 		return nativeBooltoBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
@@ -302,4 +304,17 @@ func unwrapReturnValue(obj object.Object) object.Object {
 	}
 
 	return obj
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
